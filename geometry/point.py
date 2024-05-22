15777f54d8d844f0c1a4f1695ea247464925cc7b
matthcol <matthias.colin@gmail.com>
2024-05-22 13:53:13 +0200

    Shape

--- geometry/point.py
+++ geometry/point.py
@@ -1,27 +1,49 @@
 import math
+from collections.abc import Iterable, Iterator
+from typing import Self
 
-class Point:
+from shape import Shape
+
+class Point(Shape, Iterable):
     """represents a 2D point with its coordinates x and y
     """
     
      # init method or constructor
-    def __init__(self, x=0.0, y=0.0):
+    def __init__(self, name: str|None=None, x: float=0.0, y: float=0.0):
+        super().__init__(name)
         self.x = x
         self.y = y
         
     # override both __repr__ and __str__
-    def __repr__(self):
-        return f"Point(x={self.x}, y={self.y})"
+    def __repr__(self) -> str:
+        return f"Point(name={self.name}, x={self.x}, y={self.y})"
     
-    def distance(self, p):
-        return math.sqrt((self.x - p.x)**2 +(self.y - p.y)**2)
+    def __iter__(self) -> Iterator:
+        return iter((self.x, self.y))
     
-    def translate(self, deltaX, deltaY):
+    def distance(self, p: Self) -> float:
+        return math.hypot(self.x - p.x, self.y - p.y)
+    
+    def translate(self, deltaX: float, deltaY: float):
         self.x += deltaX
         self.y += deltaY
+
+def demo_inheritance():
+    pt1 = Point(x=12, y=34)
+    print(pt1, "is an instance of class Point:", isinstance(pt1, Point))
+    print(pt1, "is an instance of class object:", isinstance(pt1, object))
+    print(pt1, "is an instance of abstract class Iterable:", isinstance(pt1, Iterable))
+    print(pt1, "is an instance of classes object, Iterable, Point:", 
+          isinstance(pt1, (object, Iterable, Point)))
+    
+def demo_iterable():
+    pt1 = Point(name="A", x=12, y=34)
+    print("Coordinates of:", pt1)
+    for coord in pt1:
+        print("\t-", coord)
         
 if __name__ == '__main__':
-    pt1 = Point(12, 34)
+    pt1 = Point(x=12, y=34)
     pt2 = Point()
     for p in pt1, pt2:
         print(p)
@@ -31,5 +53,13 @@ if __name__ == '__main__':
     print("distance:", d)
     pt1.translate(1, -1) # x=13, y=33
     print("after translate:", pt1)
+    # pt3 = Point("aa", False)
+    # print(pt3)
+    # print(pt1.distance(pt3))
+    demo_inheritance()
+    demo_iterable()
+    
+    print(Point.__mro__)
+    
         
     
--- geometry/tests/test_point.py
+++ geometry/tests/test_point.py
@@ -2,7 +2,7 @@ import pytest
 from point import Point
 
 def test_translate():
-    pt = Point(2.5, 1.25)
+    pt = Point(x=2.5, y=1.25)
     pt.translate(1.5, -0.5)
     assert pt.x == 4.0, "x"
     assert pt.y == 0.75, "y"
@@ -12,14 +12,18 @@ def test_translate():
     [
         (1.0, 2.0, 4.0, 6.0, 5.0),
         (1.5, 2.25, 5.5, -0.75, 5.0),
+        (1.5E300, 2.25E300, 5.5E300, -0.75E300, 5.0E300),
+        (1.5E-300, 2.25E-300, 5.5E-300, -0.75E-300, 5.0E-300),
     ],
     ids=[
         "integer coordinates",
         "decimal exact coordinates",
+        "big scale",
+        "tiny scale",
     ]
 )
 def test_distance(x1, y1, x2, y2, expected_distance):
-    pt1 = Point(x1, y1)
-    pt2 = Point(x2, y2)
+    pt1 = Point(x=x1, y=y1)
+    pt2 = Point(x=x2, y=y2)
     actual_distance = pt1.distance(pt2)
     assert expected_distance == actual_distance
